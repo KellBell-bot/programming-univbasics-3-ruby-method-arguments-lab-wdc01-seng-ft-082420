e2d46df0ad5b4bb131041bf781bf2f636226a270
Kellie Hamilton <kl.hamilton13@gmail.com>
2020-08-08 23:58:08 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -15,6 +15,7 @@ introduction_with_language("Kellie", "Ruby")
 def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+
 introduction_with_language_optional("Kellie")
 
 
